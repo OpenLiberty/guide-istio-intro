6bc77e570789c05b5bf2b2b5c3a51f6f8f0a070d
Panagiotis Roubatsis <proubatsis@gmail.com>
2018-06-12 11:26:51 -0400

    Copy changes to start

--- start/inventory/src/main/java/io/openliberty/guides/inventory/InventoryResource.java
+++ start/inventory/src/main/java/io/openliberty/guides/inventory/InventoryResource.java
@@ -22,7 +22,6 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import org.eclipse.microprofile.opentracing.Traced;
 
 import io.openliberty.guides.inventory.model.InventoryList;
 
