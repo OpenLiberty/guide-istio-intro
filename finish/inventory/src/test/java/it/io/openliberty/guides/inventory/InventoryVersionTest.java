b6c3749f1c69ef8f58ae6ab6a3211d9097c75705
Panagiotis Roubatsis <proubatsis@gmail.com>
2018-06-14 11:55:10 -0400

    Apply feedback from demo

--- finish/inventory/src/test/java/it/io/openliberty/guides/inventory/InventoryVersionTest.java
+++ finish/inventory/src/test/java/it/io/openliberty/guides/inventory/InventoryVersionTest.java
@@ -27,7 +27,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class InventoryEndpointTest {
+public class InventoryVersionTest {
 
     private static String testIp;
     private static String testPort;
