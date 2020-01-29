667f19a32c2eb171d06f1d7f82a9c3b38f8c7272
Kartik Saravana Arvind <kartikrajkanna@gmail.com>
2020-01-29 11:10:21 -0500

    copyrights, unneccessary imports, dockerfile format

--- finish/src/main/java/io/openliberty/guides/system/SystemResource.java
+++ finish/src/main/java/io/openliberty/guides/system/SystemResource.java
@@ -1,6 +1,6 @@
 // tag::copyright[]
 /*******************************************************************************
- * Copyright (c) 2017, 2019 IBM Corporation and others.
+ * Copyright (c) 2017, 2019, 2020 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -14,15 +14,12 @@ package io.openliberty.guides.system;
 
 // CDI
 import javax.enterprise.context.RequestScoped;
-import javax.inject.Inject;
 import javax.ws.rs.GET;
 // JAX-RS
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-
-import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @RequestScoped
 @Path("/properties")
