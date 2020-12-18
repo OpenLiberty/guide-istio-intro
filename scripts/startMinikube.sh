bb1256d676541690429f25ca5a7e531d26d47706
Austin Seto <austin.seto@ibm.com>
2020-12-18 14:48:39 -0500

    Add back conntrack installation

--- scripts/startMinikube.sh
+++ scripts/startMinikube.sh
@@ -7,6 +7,9 @@ ln -s -f $(pwd)/kubectl /usr/local/bin/kubectl
 wget https://github.com/kubernetes/minikube/releases/download/v0.28.2/minikube-linux-amd64 -q -O minikube
 chmod +x minikube
 
+sudo apt-get update -y
+sudo apt-get install -y conntrack
+
 minikube start --vm-driver=none --bootstrapper=kubeadm
 eval $(minikube docker-env)
 
