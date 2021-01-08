99fe0dc4a84bea527e746f9cc5906a8bb1c86709
Austin Seto <austinseto@gmail.com>
2021-01-08 12:44:40 -0500

    GH Actions (#112)
    * Changes for daily builds
    
    * Make file executable
    
    * Update Dockerfile in daily builds
    
    * Update dailyBuild.sh
    
    * Update testApp.sh
    
    * Update testApp.sh
    
    * Update dailyBuild.sh
    
    * Update dailyBuild.sh
    
    * Update dailyBuild.sh
    
    * Update for testing docker image
    
    * Update setup/teardown, check for ingress port
    
    * Skip confirmation on uninstall
    
    * Update curl command to match one in guide
    
    * Update flags during istio installation
    
    * Apply other yaml
    
    * Update test script to match guide README more closely
    
    * Change yaml used
    
    * kubectl set image command
    
    * Update log retrieval
    
    * Remove use of sudo
    
    * Add back conntrack installation
    
    * Manually set image for blue deployment
    
    * Fix filename
    
    * Update test.yml
    
    Co-authored-by: Austin Seto <austin.seto@ibm.com>
    Co-authored-by: Tien Thanh Le <44335153+tt-le@users.noreply.github.com>
    Co-authored-by: Gilbert Kwan <gkwan@ca.ibm.com>

---
+++ scripts/installIstio.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+ISTIO_LATEST=1.8.1
+
+curl -L https://github.com/istio/istio/releases/download/$ISTIO_LATEST/istio-$ISTIO_LATEST-linux-amd64.tar.gz | tar xzf -
+
+cd istio-$ISTIO_LATEST
+
+chmod +x bin/istioctl
+
+export PATH=$PWD/bin:$PATH
+
+istioctl install --set profile=demo -y
+
+echo "Installed Istio $(istioctl version)"
+
+sleep 240
+
+kubectl get deployments -n istio-system
+
+kubectl label --overwrite=true namespace default istio-injection=enabled
+
+cd ..
---
+++ scripts/testApp.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -euxo pipefail
+
+# Set up
+. ../scripts/startMinikube.sh
+. ../scripts/installIstio.sh
+
+# Deploy
+
+mvn -q clean package
+
+docker pull openliberty/open-liberty:kernel-java8-openj9-ubi
+docker build -t system:2.0-SNAPSHOT .
+
+kubectl apply -f system.yaml
+kubectl apply -f traffic.yaml
+
+kubectl set image deployment/system-deployment-blue system-container=system:2.0-SNAPSHOT
+kubectl set image deployment/system-deployment-green system-container=system:2.0-SNAPSHOT
+
+sleep 120
+
+kubectl get deployments
+
+kubectl get pods
+
+export INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].nodePort}')
+
+echo $(minikube ip):$INGRESS_PORT
+
+curl -H "Host:example.com" -I http://$(minikube ip):$INGRESS_PORT/system/properties
+
+# Run tests
+
+mvn test-compile
+mvn failsafe:integration-test -Ddockerfile.skip=true -Dcluster.ip=`minikube ip` -Dport=$INGRESS_PORT
+mvn failsafe:verify
+
+# Print logs
+
+POD_NAMES=($(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep system))
+
+for pod in ${POD_NAMES[@]}; do
+    kubectl logs $pod --all-containers=true
+done
+
+# Tear down
+
+. ../scripts/stopMinikube.sh
