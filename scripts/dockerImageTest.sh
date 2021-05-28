e768758dd887ed9377a60f8dd01105227017648a
Rutav Shah <rutav.shah@ibm.com>
2021-05-28 12:13:11 -0400

    fixed linting issues

--- scripts/dockerImageTest.sh
+++ scripts/dockerImageTest.sh
@@ -10,7 +10,7 @@ done
 
 echo "Testing latest OpenLiberty Docker image"
 
-sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/"$DATE"/"$DRIVER"</runtimeUrl></install></configuration>" pom.xml
+sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/""$DATE""/""$DRIVER""</runtimeUrl></install></configuration>" pom.xml
 cat pom.xml
 
 sed -i "s;FROM openliberty/open-liberty:full-java11-openj9-ubi;FROM openliberty/daily:latest;g" Dockerfile
