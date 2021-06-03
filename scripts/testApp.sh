cc5a74dd4e2f8b369647fa9f112a807d3e7846ee
Gilbert Kwan <gkwan@ca.ibm.com>
2021-06-03 12:06:53 -0400

    Update testApp.sh

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -39,8 +39,8 @@ mvn failsafe:verify
 
 # Print logs
 
-POD_NAMES=$(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep system)
-
+PODS=$(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{","}')
+IFS=',' read -r -a POD_NAMES <<< "$PODS"
 for pod in ${POD_NAMES[@]}; do
     kubectl logs "$pod" --all-containers=true
 done
