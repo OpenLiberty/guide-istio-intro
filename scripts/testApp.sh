923b4dbe4febd510b7a3befded5d2d333e49e648
Gilbert Kwan <gkwan@ca.ibm.com>
2021-06-03 12:51:10 -0400

    Update testApp.sh

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -21,24 +21,19 @@ kubectl set image deployment/system-deployment-green system-container=system:2.0
 sleep 120
 
 kubectl get deployments
-
 kubectl get pods
 
 export INGRESS_PORT
 INGRESS_PORT="$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].nodePort}')"
-
 echo "$(minikube ip)":"$INGRESS_PORT"
-
 curl -H "Host:example.com" -I http://"$(minikube ip)":"$INGRESS_PORT"/system/properties
 
 # Run tests
-
 mvn test-compile
 mvn failsafe:integration-test -Ddockerfile.skip=true -Dcluster.ip="$(minikube ip)" -Dport="$INGRESS_PORT"
 mvn failsafe:verify
 
 # Print logs
-
 PODS=$(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{","}')
 IFS=',' read -r -a POD_NAMES <<< "$PODS"
 for pod in "${POD_NAMES[@]}"; do
@@ -47,12 +42,5 @@ for pod in "${POD_NAMES[@]}"; do
     fi
 done
 
-#POD_NAMES=$(kubectl get pods --no-headers -o custom-columns=":metadata.name" | grep system)
-#read -r -d '' -a arr <<<"${POD_NAMES}"
-#for (( i=0; i<${#arr[@]}; i++ )); do
-#    kubectl logs "${arr[$i]}" --all-containers=true
-#done
-
 # Tear down
-
 ../scripts/stopMinikube.sh
