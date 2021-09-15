45c168ce9b42020e60f4c2826207d9c04c47dfb9
Gilbert Kwan <gkwan@ca.ibm.com>
2021-09-15 15:10:47 -0400

    Update installIstio.sh

--- scripts/installIstio.sh
+++ scripts/installIstio.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 ISTIO_LATEST=1.11.2
 
+apt install socat
+
 curl -L https://github.com/istio/istio/releases/download/$ISTIO_LATEST/istio-$ISTIO_LATEST-linux-amd64.tar.gz | tar xzf -
 
-cd istio-$ISTIO_LATEST || exit
+cd istio-$ISTIO_LATEST || exit 1
 
 chmod +x bin/istioctl
 
